5416021ec0338ee2f9270cf639db10de5f490526
ziyatov-zafar-dev <zziyatov811@gmail.com>
2025-12-19 05:16:52 +0500

    files

--- src/main/java/uz/codebyz/storage/WebConfig.java
+++ src/main/java/uz/codebyz/storage/WebConfig.java
@@ -8,15 +8,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     private final String uploadsRoot;
+
     public WebConfig(@Value("${storage.profile.base.url}") String profileImageDir) {
         java.nio.file.Path dir = java.nio.file.Paths.get(profileImageDir).toAbsolutePath().normalize();
         java.nio.file.Path root = dir.getParent() != null ? dir.getParent() : dir;
         this.uploadsRoot = root.toString();
     }
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         String location = "file:" + uploadsRoot + "/";
-        registry.addResourceHandler("/uploads/**")
+        registry.addResourceHandler("/files/**")
                 .addResourceLocations(location);
     }
 }
